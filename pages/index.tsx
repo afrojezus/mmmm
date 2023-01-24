d86e7760561510bcdb1e5097c2d09271d68926bc
afrojezus <thoralf21@gmail.com>
2023-01-25 00:11:49 +0100

    add a ton of meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,16 @@ export default function Home() {
         <meta name="description" content="mmmmmmmmmm" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="mmmmmmmmmmmmmm" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="mmmm.moe" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content="mmmmmmmmmmm" />
+        <meta property="og:image" content="/mmmm.webp" />
+        <meta property="og:image:type" content="image/webp"/>
+        <meta property="og:image:width" content="250"/>
+        <meta property="og:image:height" content="250" />
+        <meta name="theme-color" content="#ff7be9" />
       </Head>
       <Image className="mmmmm" src="/mmmm.webp" alt="mmmmm" fill />
     </>
